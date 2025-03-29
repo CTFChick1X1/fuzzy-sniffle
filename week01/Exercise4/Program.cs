91f19dece0b65c4c7e03407c63c59cf35f7feded
CTFChick1X1 <mwangiessy2007@gmail.com>
2025-03-29 13:17:55 +0300

    change .NET Framework to net9.0

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,50 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+        int number;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        do
+        {
+            Console.Write("Enter number: ");
+            number = int.Parse(Console.ReadLine());
+
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+
+        } while (number != 0);
+
+        // Compute sum
+        int sum = 0;
+        foreach (int num in numbers)
+        {
+            sum += num;
+        }
+        Console.WriteLine("");
+        Console.WriteLine($"The sum is: {sum}");
+
+        // Compute average
+        double average = (double)sum / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        // Find max number
+        int maxNumber = numbers[0];
+        foreach (int num in numbers)
+        {
+            if (num > maxNumber)
+            {
+                maxNumber = num;
+            }
+        }
+        Console.WriteLine($"The largest number is: {maxNumber}");
     }
 }
+// This program allows the user to enter a list of numbers, computes the sum, average, and finds the largest number.
