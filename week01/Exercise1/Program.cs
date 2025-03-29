744d570da789d0cbd8bdc6bd56ae00eb2f03e1a1
CTFChick1X1 <mwangiessy2007@gmail.com>
2025-03-29 13:09:16 +0300

    Exercise1, .NET version changed to 9.0 since that the version i have

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -1,9 +1,28 @@
+
 using System;
 
+// Declare and initialize variables for first name and last name
+
+// dotnet sln CSE210.sln add week01/Exercise2/Exercise2.csproj
+
 class Program
+
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        // Declare variables for first name and last name
+        string firstname;
+        string lastname;
+
+        // Ask for user input for first name and last name
+        Console.Write("What is your first name: ");
+        firstname = Console.ReadLine() ?? "Unknown";
+
+        Console.Write("What is your last name: ");
+        lastname = Console.ReadLine() ?? "Unknown";
+
+        // Write both last name and first name in the same line
+        Console.WriteLine($"Your name is {lastname},  {firstname} {lastname}!");
     }
 }
+
