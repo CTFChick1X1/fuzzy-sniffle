8debf21f3cb20b01ca15ecf8810fc298e7b9ade8
CTFChick1X1 <mwangiessy2007@gmail.com>
2025-04-05 08:53:34 +0300

    inheritance

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -1,9 +1,24 @@
-using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+
+        // Create Assignment instance and display its information
+        Console.WriteLine("\n");
+        Assignment a1 = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(a1.GetSummary());
+        Console.WriteLine("\n");
+
+        // Create a MathAssignment instance and display its summary and homework list
+        MathAssignment math = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(math.GetSummary());
+        Console.WriteLine(math.GetHomeworkList());
+        Console.WriteLine("\n");
+
+        // Create a WritingAssignment instance and display its summary and writing information
+        WritingAssignment writing = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(writing.GetSummary());
+        Console.WriteLine(writing.GetWritingInformation());
     }
 }
+// Output:
